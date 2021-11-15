bba1af030d6ceb8e780e768f12056b7a79ab0d38
Jose Fco Martinez <joframale@gmail.com>
2021-11-15 22:23:56 +0100

    Iniciado la AA de Acceso a datos, subiendo ficheros

---
+++ src/main/java/com/josefco/accesoadatosaa/AccesoadatosaaApplication.java
@@ -0,0 +1,13 @@
+package com.josefco.accesoadatosaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesoadatosaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesoadatosaaApplication.class, args);
+	}
+
+}
