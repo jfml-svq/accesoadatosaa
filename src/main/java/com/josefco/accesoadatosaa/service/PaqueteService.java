5c6fd05ae1fb1b0b96638164cfadc6f1e2e2cfe2
Jose Fco Martinez <joframale@gmail.com>
2021-11-16 21:55:05 +0100

    Completadas todas las clases, con sus relaciones y el CRUD de todas v2

--- src/main/java/com/josefco/accesoadatosaa/service/PaqueteService.java
+++ src/main/java/com/josefco/accesoadatosaa/service/PaqueteService.java
@@ -1,8 +1,6 @@
 package com.josefco.accesoadatosaa.service;
 
-import com.josefco.accesoadatosaa.domain.Camion;
 import com.josefco.accesoadatosaa.domain.Paquete;
-import com.josefco.accesoadatosaa.exception.CamionNoEncontradoException;
 import com.josefco.accesoadatosaa.exception.PaqueteNoEncontradoException;
 
 import java.util.List;
@@ -13,7 +11,7 @@ public interface PaqueteService {
     List<Paquete> findAllPaquetes();
     Paquete findPaquete(int id) throws PaqueteNoEncontradoException;
 
-    Paquete addPaquete(Paquete Paquete);
+    Paquete addPaquete(Paquete paquete);
     Paquete deletePaquete(int id) throws PaqueteNoEncontradoException;
     Paquete modifyPaquete(int id, Paquete Paquete) throws PaqueteNoEncontradoException;
 }
--- src/main/java/com/josefco/accesoadatosaa/service/PaqueteServiceImpl.java
+++ src/main/java/com/josefco/accesoadatosaa/service/PaqueteServiceImpl.java
@@ -3,7 +3,9 @@ package com.josefco.accesoadatosaa.service;
 
 import com.josefco.accesoadatosaa.domain.Paquete;
 import com.josefco.accesoadatosaa.exception.PaqueteNoEncontradoException;
+import com.josefco.accesoadatosaa.repository.ConductorRepository;
 import com.josefco.accesoadatosaa.repository.PaqueteRepository;
+import com.josefco.accesoadatosaa.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,10 @@ public class PaqueteServiceImpl implements PaqueteService {
 
     @Autowired
     private PaqueteRepository paqueteRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private ConductorRepository conductorRepository;
 
     @Override
     public List<Paquete> findAllPaquetes() {
@@ -30,6 +36,7 @@ public class PaqueteServiceImpl implements PaqueteService {
     public Paquete addPaquete(Paquete Paquete) {
         return paqueteRepository.save(Paquete);
     }
+
 
     @Override
     public Paquete deletePaquete(int id) throws PaqueteNoEncontradoException {
